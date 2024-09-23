21b70ec9fe94a473a98709dedaa505fe52870bc9
joshuaadampoirier <josh.poirier@gmail.com>
2024-09-23 00:39:23 -0400

    fix: expose value validators to get captured in docs

--- nlca_pipelines/validation/__init__.py
+++ nlca_pipelines/validation/__init__.py
@@ -1,3 +1,29 @@
-from .validate import validate  # type: ignore
+# pylint: disable=R0801
 
-__all__ = ["validate"]
+from .validate import validate  # type: ignore
+from .values import (
+    Basin,
+    County,
+    Cum12mgas,
+    Cum12moil,
+    Cum12mwater,
+    Direction,
+    Spuddate,
+    State,
+    Subbasin,
+    Welltype,
+)
+
+__all__ = [
+    "validate",
+    "Basin",
+    "County",
+    "Cum12mgas",
+    "Cum12moil",
+    "Cum12mwater",
+    "Direction",
+    "Spuddate",
+    "State",
+    "Subbasin",
+    "Welltype",
+]
