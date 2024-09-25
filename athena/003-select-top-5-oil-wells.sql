b36c5c44ac1e6f0790ab34ce7332f1975023b7b0
joshuaadampoirier <josh.poirier@gmail.com>
2024-09-25 00:14:22 -0400

    chore: delete old sql files

--- athena/003-select-top-5-oil-wells.sql
+++
@@ -1,6 +0,0 @@
-SELECT      api10,
-            cum12moil
-FROM        silver.wells
-WHERE       welltype = 'OIL'
-ORDER BY    cum12moil DESC
-LIMIT       5;
--- athena/004-sum-prod-by-basin.sql
+++
@@ -1,6 +0,0 @@
-SELECT      basin,
-            SUM(cum12moil) AS cum12moil,
-            SUM(cum12mgas) AS cum12mgas,
-            SUM(cum12mwater) AS cum12mwater
-FROM        silver.wells
-GROUP BY    basin;
